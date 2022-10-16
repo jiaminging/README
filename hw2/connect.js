7d90e1773efa7b3880e803dfc129a61c5ddf9b5e
jiaminging <h23738536@gmail.com>
2022-10-16 23:23:14 +0800

    2

--- hw2/connect.js
+++ hw2/connect.js
@@ -4,10 +4,11 @@ var con = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "Oswald.0704",
-});
+})
 
 con.connect(function(err){
-
-    if (err) throw err;
+    if(err) throw err;
     console.log("Connected!");
-});
+
+})
+
