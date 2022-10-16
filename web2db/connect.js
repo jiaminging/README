c997cdaeec5423b773fafacaf36d5dcaac319b36
jiaminging <h23738536@gmail.com>
2022-10-16 16:50:10 +0800

    1

---
+++ web2db/connect.js
@@ -0,0 +1,13 @@
+var mysql = require('mysql')
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "Oswald.0704",
+});
+
+con.connect(function(err){
+
+    if (err) throw err;
+    console.log("Connected!");
+});
